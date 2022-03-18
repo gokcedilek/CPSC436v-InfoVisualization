a742d13bb90f365f0543e6c61e65e3fe0a328edc
Evelyn Chen <evelynywchen@gmail.com>
2022-03-17 22:20:57 -0700

    Global data filtering: country dropdown

--- js/chord.js
+++ js/chord.js
@@ -1,8 +1,21 @@
 class ChordDiagram {
 
-    constructor() {
-  
+  /**
+   * Class constructor with initial configuration
+   * @param {Object}
+   * @param {Array}
+   */
+   constructor(_config, _data) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: 1000,
+      containerHeight: 500,
+      margin: {top: 30, right: 30, bottom: 30, left: 30},
+      tooltipPadding: 10
     }
+    this.data = _data;
+    this.initVis();
+  }
   
     initVis() {
       // Create SVG area, initialize scales and axes
