24e701b1de0e30e0b645d90e3491cfe7703d055d
cacxy@student.ubc.ca <cacxy@student.ubc.ca>
2022-03-25 20:55:21 -0700

    resolved bubble location issue

--- js/bubble.js
+++ js/bubble.js
@@ -20,7 +20,9 @@ class BubbleDiagram {
     }
     this.fullData = _data;
     this.data = _data.filter((d) => d['GENERAL_EVENT_GROUP'] == this.config.generalEventGroup);
-    // this.dispatcher = _dispatcher
+    this.radius_min = 4;
+    this.radius_max = 80;
+    this.dispatcher = _dispatcher
     this.initVis();
   }
   
@@ -49,12 +51,8 @@ class BubbleDiagram {
         // note we have to ensure that size is a number
         const allGroupsSrc = d3.rollup(vis.fullData, v => v.length, d => d.GENERAL_EVENT_GROUP,d => d.SOURCE_SCALE)
         vis.allBubbles = this.createNodes(allGroupsSrc)
-        const maxSize = d3.max(vis.allBubbles, d => +d.size);
-
-        // size bubbles based on area
-        vis.radiusScale = d3.scaleSqrt()
-            .domain([0, maxSize])
-            .range([0, 80])
+        vis.event_num_max = d3.max(vis.allBubbles, d => +d.size);
+        vis.event_num_min = d3.min(vis.allBubbles, d => +d.size);
 
         // set up colour scale
         vis.fillColour = d3.scaleOrdinal()
@@ -82,8 +80,8 @@ class BubbleDiagram {
                 let node = {
                     name: key,
                     size: value,
-                    x: Math.random() * 900,
-                    y: Math.random() * 800
+                    x: Math.random() * 333,
+                    y: Math.random() * 333
                 }
                 myNodes.push(node)
             }
@@ -92,9 +90,21 @@ class BubbleDiagram {
         return myNodes;
     }
 
+    radiusScale(d) {
+        let vis = this;
+
+        d = Math.sqrt(d)
+        let min = Math.sqrt(vis.event_num_min)
+        let max = Math.sqrt(vis.event_num_max)
+        let radius = vis.radius_min + (d-min)/(max-min) * (vis.radius_max - vis.radius_min)
+        return radius
+    }
+
+
     // charge is dependent on size of the bubble, so bigger towards the middle
     charge(d) {
-        return Math.pow(d.size, 2.0) * 0.01
+       let forceSize = this.radiusScale(d.size)
+        return Math.pow(forceSize, 2.0) * 0.01
     }
   
     updateVis() {
@@ -105,7 +115,7 @@ class BubbleDiagram {
 
         // create a force simulation and add forces to it
         vis.simulation = d3.forceSimulation()
-            .force('charge', d3.forceManyBody().strength(this.charge))
+            .force('charge', d3.forceManyBody().strength(this.charge(vis)))
             // .force('center', d3.forceCenter(centre.x, centre.y))
             .force('x', d3.forceX().strength(vis.config.forceStrength).x(vis.centre.x))
             .force('y', d3.forceY().strength(vis.config.forceStrength).y(vis.centre.y))
@@ -134,10 +144,11 @@ class BubbleDiagram {
                     d3.select(this).classed('active', !isActive)
                         .style('stroke-width', 3 );
                     // Get the names of all active/filtered categories
-                    const selectedInfoSourceEvents = vis.svg.selectAll('.bubble.active').data().map(k => k.key);
+                    const selectedEvents = vis.config.generalEventGroup;
+                    const selectedInfoSource = vis.svg.selectAll('.bubble.active').data()[0]['name'];
                     // Call dispatcher and pass the event name, D3 event object,
                     // and our custom event data (selected category names)
-                    // vis.dispatcher.call('filteredInfoSourceEvent', event, selectedInfoSourceEvents);
+                    vis.dispatcher.call('filteredInfoSourceEvent', event, selectedEvents, selectedInfoSource);
                 })
 
         // set simulation's nodes to our newly created nodes array
